b987050d56c37e45082fe95b1ee39738c8864e6e
MuhammadAbdullahWarraich <muhammadabdullah2016pk@gmail.com>
2024-08-03 15:28:34 +0500

    fix: add minor details

--- src/scripts/initial-page-load.js
+++ src/scripts/initial-page-load.js
@@ -14,25 +14,31 @@ export default function initialLoad() {
         return image;
     })());
 
-    const hours = component('ul', (function() {
-        let str = '';
-        const timings = [
-            'Monday to Thursday - 6 am to 10pm',
-            'Weekends (Fri - Sun) - 8am to 12am',
-        ];
-        for (const el of timings) {
-            str += `<li>${el}</li>`;
-        }
-        return str;
-    })());
+    children.push(component('h2', "Hours"));
+    const hours = listComponent('ul', [
+        'Monday to Thursday - 6 am to 10pm',
+        'Weekends (Fri - Sun) - 8am to 12am',
+    ]);
     children.push(hours);
+
+    children.push(component('h2', "Locations"));
+    children.push(listComponent('ol', [
+        "123 Programmer Street,  Alpha Colony, Goofy Country",
+        "321-A xyz Street, Beta Town, Awesome City, Earth"
+    ]));
 
     const content = document.getElementById('content');
     for (const child of children) {
         content.appendChild(child);
     }
 }
-
+function listComponent(tag, items) {
+    let str = "";
+    for (const li of items) {
+        str += `<li>${li}</li>`;
+    }
+    return component(tag, str);
+}
 function component(tag, content) {
     const el = document.createElement(tag);
     if (content !== undefined) el.innerHTML = content;
